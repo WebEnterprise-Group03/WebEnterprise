ef8b5a55af5d9ac40e143751463b9860002c11ff
Huu Tu <huutu7165@gmail.com>
2022-02-24 18:43:35 +0700

    Huutu commit
    created api login and register

---
+++ server/src/controllers/accountController.js
@@ -0,0 +1,54 @@
+const Account = require('../models/accountModels');
+
+class accountController{
+  index(req,res){
+    res.json('Home')
+  }
+
+  register(req ,res){
+    let username = req.body.username;
+    let password = req.body.password;
+
+    Account.findOne({
+      username: username
+    })
+      .then(data =>{
+        if (data){
+          res.json('User nay da ton tai');
+        }else {
+          return Account.create({
+            username: username,
+            password: password,
+          })
+        }
+      })
+      .then(data =>{
+        res.json('Ban da tao tk thanh cong');
+      })
+      .catch(err =>{
+        res.status(500).json('Ban tao tk thao bai');
+      })
+  }
+
+  login(req,res){
+    let username = req.body.username;
+    let password = req.body.password;
+
+    Account.findOne({
+      username: username,
+      password: password,
+    })
+      .then(data =>{
+        if(data){
+          res.json('Dang nhap thanh cong');
+        }else {
+          return res.json('Thap bai');
+        }
+      })
+      .catch(err =>{
+        res.status(500).json('Loi server', err)
+      })
+  }
+}
+
+module.exports = new accountController()
--- server/src/routes/accountRouter.js
+++ server/src/routes/accountRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const accountController = require('../controllers/accountController');
 
-router.get('/',(req,res)=>{
-  res.json('Hello ca nha');
-})
+router.post('/register', accountController.register);
+router.post('/login', accountController.login);
+router.get('/',accountController.index);
 
 module.exports = router;
