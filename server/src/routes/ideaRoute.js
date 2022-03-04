8063b792e1539bd85922bf7c0c7446e5afafc561
Huu Tu <huutu7165@gmail.com>
2022-03-04 13:59:45 +0700

    huutu commit

--- server/src/routes/ideaRoute.js
+++ server/src/routes/ideaRoute.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const IdeaController = require('../controllers/ideaController');
 const Upload = require('../middlewares/upload');
 
-Upload.array('docs', 5)
 
 router.get('/detail/:slug', IdeaController.detail);
 router.get('/create', IdeaController.create);
-router.post('/store', Upload.array('file', 5),IdeaController.store);
+router.post('/store', Upload.single('file'),IdeaController.store);
 router.get('/:id/update', IdeaController.update);
 router.put('/:id', IdeaController.updateIdea);
 router.delete('/:id', IdeaController.deleteIdea);
