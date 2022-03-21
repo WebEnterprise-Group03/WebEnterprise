c048c62ae428ae114ef8f940878c8bc1260878fb
LuongTran31 <86140948+LuongTran31@users.noreply.github.com>
2022-03-22 01:48:46 +0700

    comment

--- server/src/routes/cmtRoute.js
+++ server/src/routes/cmtRoute.js
@@ -3,5 +3,5 @@ const cmtController = require('../controllers/commentController.js');
 const router = express.Router();
 
 router.get('/create', cmtController.create);
-router.post('/detail/:slug/comment',cmtController.create);
+router.post('/detail/:slug/comment',cmtController.store);
 module.exports = router;
