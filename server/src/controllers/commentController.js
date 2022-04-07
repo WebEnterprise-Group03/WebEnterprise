4cc4ed7024e4f24c4b7833b7b1b483cba3d23ee7
Huu Tu <huutu7165@gmail.com>
2022-04-07 15:27:56 +0700

    completed deadline

--- server/src/controllers/commentController.js
+++ server/src/controllers/commentController.js
@@ -1,30 +1,37 @@
 const Comment = require('../models/cmtModel.js');
 const Idea = require('../models/ideaModel');
 const Account = require('../models/accountModel');
+const Task = require('../models/taskModel');
 
 class cmtController {
   async doComment(req, res, next) {
+    const DateNow = Date.now();
+    const getCurrentDate = new Date(DateNow);
     const { data } = req.data.username;
     const { _id } = req.body;
+    // const title = "TestDealine";
     const formData = {
       content: req.body.content,
     };
 
+
     const IdeaPost = await Idea.findOne({ _id: _id });
     const author = await Account.findOne({ username: req.data.username });
+    const TaskDealine = await Task.findOne({ title: "SetDeadlineComment" });
+    const endDate = TaskDealine.endDate;
 
     formData.idea = IdeaPost._id;
     formData.author = author._id;
     Comment.create(formData, (err, item) => {
-      if (err) {
-        console.log(err);
-      } else {
-        item.save();
+        if (getCurrentDate > endDate){
+          item.save();
 
-        IdeaPost.comments.push(item);
-        IdeaPost.save();
-        res.redirect('back');
-      }
+          IdeaPost.comments.push(item);
+          IdeaPost.save();
+          res.redirect('back');
+        }else {
+          console.log('Da den deadline');
+        }
     });
   }
 
